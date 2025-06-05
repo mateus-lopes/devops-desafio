9477f65bbcd62aad2d7c79e917e97bc2c8fe6190
mateus-lopes <mateusalbano22@gmail.com>
2025-06-05 10:57:51 -0300

    fix: testing main.py

--- test_main.py
+++ test_main.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello World 2"}
 
 
 def test_square():
